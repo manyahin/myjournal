9faeafc14be119eb7bad4e883f6ba3da452fd17a
Ilya Manyahin <i.manyahin@gmail.com>
2018-12-20 22:37:24 +0200

    make note component

---
+++ client/src/components/Note.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="note">
+    <div class="header pure-g">
+      <div class="time pure-u-1-2">{{ note.created_at | moment('HH:mm') }}</div>
+      <div class="actions pure-u-1-2">
+        <!-- make like component -->
+        <img @click="favorite(note)" :src="'/static/' + (note.favorite ? 'star-48.png' : 'star-filled-48.png')" alt="star" class="star-icon">
+      </div>
+    </div>
+    <div class="body">{{ note.body }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    note: {
+      type: Object
+    }    
+  }
+}
+</script>
+
+<style scoped>
+.actions {
+  text-align: right;
+}
+.note {
+  padding: 10px 0;
+  border-bottom: 1px solid rgba(1,1,1,0.1);
+}
+.note .body {
+  white-space: pre-line;
+  line-height: 1.3em;
+}
+.note .time {
+  color: grey;
+  line-height: 26px;
+}
+.star-icon {
+  width: 24px;
+  height: 24px;
+  opacity: 0.5;
+}
+.star-icon:hover {
+  opacity: 1;
+}
+</style>
--- client/src/components/NotesList.vue
+++ client/src/components/NotesList.vue
@@ -5,13 +5,7 @@
         <h4>{{ new Date(parseInt(index)) | moment("dddd, MMM Do YY") }}</h4>
         <ul>
           <li v-for="(note, i) in notes" :key="i">
-            <div class="note">
-              <div class="time">{{ note.created_at | moment('HH:mm') }}</div>
-              <div class="body">{{ note.body }}</div>
-              <div class="footer">
-                <img @click="favorite(note)" :src="'/static/' + (note.favorite ? 'star-48.png' : 'star-filled-48.png')" alt="star" class="star-icon">
-              </div>
-            </div>
+            <note :note="note"></note>
           </li>
         </ul>
       </li>
@@ -21,7 +15,12 @@
 
 <script>
 
+import Note from '@/components/Note'
+
 export default {
+  components: {
+    Note
+  },
   props: {
     notes: {
       type: Array
@@ -56,8 +55,8 @@ export default {
 
 <style scoped>
 h4 {
-  margin-top: 2px;
-  margin-bottom: 12px;
+  margin-top: 10px;
+  margin-bottom: 0px;
 }
 .notes {
   word-wrap: break-word
@@ -68,31 +67,5 @@ ul {
 }
 ul li {
   /*border: 1px solid rgba(1,1,1,0.2);*/
-}
-.note {
-  margin-bottom: 8px;
-  line-height: 1.3em;
-  white-space: pre-line;
-  border-bottom: 1px solid rgba(1,1,1,0.1);
-}
-.note .time {
-  margin-bottom: 4px;
-  color: grey;
-  font-size: 14px;
-  /*font-style: italic;*/
-}
-.note .body {
-  margin-bottom: 6px;
-}
-.note .footer {
-  margin-bottom: 6px;
-}
-.star-icon {
-  width: 24px;
-  height: 24px;
-  opacity: 0.5;
-}
-.star-icon:hover {
-  opacity: 1;
 }
 </style>
