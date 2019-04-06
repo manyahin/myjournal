33ee5d4833dcf773bbdc47b57bb0b3ff0296b93f
Ilya Manyahin <i.manyahin@gmail.com>
2019-04-06 14:08:33 +0300

    use infinity scroll on read page fot all actions

--- client/src/components/ReadPage.vue
+++ client/src/components/ReadPage.vue
@@ -4,73 +4,117 @@
     <div class="filters pure-g">
       <div class="pure-u-1 pure-u-sm-1-2">
         Filters:
-        <button @click="loadAllNotes">All notes</button>
-        <button @click="loadOnlyStarredNotes">Only starred notes</button>
+        <button @click="readAllNotes">All notes</button>
+        <button @click="readStarredNotes">Only starred notes</button>
       </div>
-      <div class="pure-u-1 pure-u-sm-1-2">
-        <search @onSubmit="loadBySearch"></search>
+      <div class="pure-u-1 pure-u-sm-1-2 search-form">
+        <search @onSubmit="readBySearch"></search>
       </div>
     </div>
-    <loading :status="loading"></loading>
     <notes-list :notes="notes" :loading="loading"></notes-list>
+    <infinite-loading :identifier="infiniteId" @infinite="infiniteHandler">
+      <div slot="no-more" class="no-more-notes">All notes loaded</div>
+      <div slot="no-results"></div>
+      <div slot="spinner">
+        <loading :status="true"></loading>
+      </div>
+    </infinite-loading>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import InfiniteLoading from 'vue-infinite-loading'
+
 import NotesList from '@/components/NotesList'
 import Loading from '@/components/Loading'
 import Search from '@/components/Search'
+
+const NOTES_PER_PAGE = 50
 
 export default {
   components: {
     NotesList,
     Loading,
-    Search
+    Search,
+    InfiniteLoading
   },
   data () {
     return {
       notes: [],
-      loading: false
+      page: 0,
+      loading: true,
+      view: 'all',
+      searchText: '',
+      infiniteId: +new Date()
     }
   },
-  async created () {
-    await this.loadAllNotes()
-  },
   methods: {
-    async loadOnlyStarredNotes () {
-      this.beforeLoad()
-      let filter = {
-        where: {
-          favorite: true
-        },
-        order: 'created_at DESC'
-      }
-      let { data } = await axios.get('Notes?filter=' + JSON.stringify(filter))
-      this.notes = data
-      this.afterLoad()
+    async loadNotes (filter = {}) {
+      const { data } = await axios.get(`Notes?filter=${JSON.stringify(filter)}`)
+      return data
     },
-    async loadAllNotes () {
+    async readAllNotes () {
+      this.view = 'all'
+      this.infiniteId += 1
       this.beforeLoad()
-      let { data } = await axios.get('Notes?filter={"order": "created_at ASC"}')
-      this.notes = data
-      this.afterLoad()
     },
-    async loadBySearch (text) {
+    async readStarredNotes () {
+      this.view = 'star'
+      this.infiniteId += 1
       this.beforeLoad()
-      let filter = {
-        where: {
-          '$text': {
-            search: text
-          }
-        }
+    },
+    async readBySearch (text) {
+      this.view = 'search'
+      this.searchText = text
+      this.infiniteId += 1
+      this.beforeLoad()
+    },
+    async infiniteHandler ($state) {
+      const notes = []
+      switch (this.view) {
+        case 'all':
+          notes.push(...await this.loadNotes({
+            order: 'created_at ASC',
+            skip: NOTES_PER_PAGE * this.page,
+            limit: NOTES_PER_PAGE
+          }))
+          break
+        case 'star':
+          notes.push(...await this.loadNotes({
+            where: {
+              favorite: true
+            },
+            order: 'created_at ASC',
+            skip: NOTES_PER_PAGE * this.page,
+            limit: NOTES_PER_PAGE
+          }))
+          break
+        case 'search':
+          notes.push(...await this.loadNotes({
+            where: {
+              '$text': {
+                search: this.searchText
+              }
+            },
+            order: 'created_at ASC',
+            skip: NOTES_PER_PAGE * this.page,
+            limit: NOTES_PER_PAGE
+          }))
+          break
       }
-      let { data } = await axios.get('Notes?filter=' + JSON.stringify(filter))
-      this.notes = data
+      if (notes.length) {
+        this.notes.push(...notes)
+        this.page += 1
+        $state.loaded()
+      } else {
+        $state.complete()
+      }
       this.afterLoad()
     },
     beforeLoad () {
       this.notes = []
+      this.page = 0
       this.loading = true
     },
     afterLoad () {
@@ -87,5 +131,22 @@ export default {
 
 .link-to-calendar {
   margin-top: 10px;
+}
+
+.no-more-notes {
+  margin-bottom: 25px;
+  color: grey;
+  font-style: italic;
+}
+
+.search-form {
+  text-align: right;
+}
+
+@media screen and (max-width: 35.5em) {
+  .search-form {
+    margin-top: 8px;
+    text-align: left;
+  }
 }
 </style>
