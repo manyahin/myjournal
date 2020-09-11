02e2eec33cf5245983eda349eeba6cb8d3f571be
Ilya Manyahin <i.manyahin@gmail.com>
2020-09-11 21:03:50 +0300

    setuyp prettier, format, webpack to share

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -2,36 +2,27 @@
   <section class="container login box">
     <h1 class="title">My Diary</h1>
     <form @submit.prevent="login">
-
       <!-- <b-field class="email-field">
-        <b-input 
+        <b-input
           v-model="email"
           icon="email"
           type="email"
-          placeholder="Email" 
+          placeholder="Email"
           required>
         </b-input>
-      </b-field> -->
+      </b-field>-->
 
       <b-field class="password-field">
-        <b-input 
-          v-model="password"
-          icon="lock"
-          type="password"
-          placeholder="Password" 
-          required>
-        </b-input>
+        <b-input v-model="password" icon="lock" type="password" placeholder="Password" required></b-input>
       </b-field>
 
       <!-- <div class="field">
         <b-checkbox v-model="rememberMe">Remember me for a week</b-checkbox>
-      </div> -->
+      </div>-->
 
       <b-button native-type="submit" type="is-primary">Sign in</b-button>
 
-      <b-message v-show="message" :type="messageType" class="custom-message">
-        {{ message }}
-      </b-message>
+      <b-message v-show="message" :type="messageType" class="custom-message">{{ message }}</b-message>
     </form>
   </section>
 </template>
@@ -41,7 +32,7 @@ import axios from 'axios'
 import _ from 'lodash'
 
 export default {
-  data () {
+  data() {
     return {
       email: 'user@mail.com',
       // rememberMe: '',
@@ -51,14 +42,15 @@ export default {
     }
   },
   methods: {
-    login (e) {
+    login(e) {
       this.resetMessage()
       this.message = 'Loading...'
 
-      axios.post('Customers/login', {
-        email: this.email,
-        password: this.password
-      })
+      axios
+        .post('Customers/login', {
+          email: this.email,
+          password: this.password
+        })
         .then(res => {
           localStorage.setItem('token', res.data.id)
 
@@ -68,11 +60,12 @@ export default {
           window.location = '/'
         })
         .catch(err => {
-          this.message = _.get(err, 'response.data.error.message') || 'Connection problem'
+          this.message =
+            _.get(err, 'response.data.error.message') || 'Connection problem'
           this.messageType = 'is-danger'
         })
     },
-    resetMessage () {
+    resetMessage() {
       this.message = ''
       this.messageType = ''
     }
@@ -82,7 +75,7 @@ export default {
 
 <style scoped>
 .title {
-  font-family: "Merriweather", "Georgia", serif;
+  font-family: 'Merriweather', 'Georgia', serif;
   margin-bottom: 30px;
 }
 .login {
